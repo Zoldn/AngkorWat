64ca74f981aaeeb0b8cf5276c4bdd9e270e9a52a
Zoldn <mihail-svetkin@mail.ru>
2022-09-12 21:17:53 +0300

    JSON input output example

---
+++ AngkorWat/AngkorWat/IO/OutputContainer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AngkorWat.IO
+{
+    internal class DerPunkt
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Z { get; set; }
+        public DerPunkt() { }
+    }
+
+    internal class OutputContainer
+    {
+        public List<DerPunkt> Punkte { get; set; }
+        public OutputContainer() 
+        {
+            Punkte = new();
+        }
+    }
+}
--- AngkorWat/AngkorWat/Program.cs
+++ AngkorWat/AngkorWat/Program.cs
@@ -1,7 +1,47 @@
-﻿internal class Program
+﻿using AngkorWat.IO;
+using Newtonsoft.Json;
+
+internal class Program
 {
-    private static void Main(string[] args)
+    private static void Main(string[] _)
     {
         Console.WriteLine("Hello, World!");
+
+        var outputContainer = new OutputContainer()
+        {
+            Punkte = new List<DerPunkt>()
+            {
+                new DerPunkt() { X = 1, Y = 2, Z = 3, },
+                new DerPunkt() { X = 4, Y = 1, Z = 2, },
+                new DerPunkt() { X = 2, Y = 0, Z = 1, },
+            }
+        };
+
+        Serialize(outputContainer);
+
+        outputContainer = Deserialize("output.json");
+    }
+
+    public static void Serialize(OutputContainer outputContainer)
+    {
+        string json = JsonConvert.SerializeObject(outputContainer);
+
+        File.WriteAllText("output.json", json);
+
+        Console.WriteLine("Collections are serialized");
+    }
+
+    public static OutputContainer Deserialize(string filepath)
+    {
+        string json = File.ReadAllText(filepath);
+
+        var container = JsonConvert.DeserializeObject<OutputContainer>(json);
+
+        if (container is null)
+        {
+            throw new FileNotFoundException();
+        }
+
+        return container;
     }
 }
