5cdae608c87c39072f40af87a76e56612fd07137
Mikhail Svetkin <mihail-svetkin@mail.ru>
2024-07-12 16:52:51 +0300

    More smart HTTP helper for GET/POST with not 200 return handling

--- AngkorWat/AngkorWat/IO/HTTP/HttpHelper.cs
+++ AngkorWat/AngkorWat/IO/HTTP/HttpHelper.cs
@@ -8,9 +8,34 @@ using System.Net;
 using System.Net.Mime;
 using System.Text;
 using System.Threading.Tasks;
+using static AngkorWat.IO.HTTP.HttpHelper;
 
 namespace AngkorWat.IO.HTTP
 {
+    public class PostResponse<TInput, TOutput, TError>
+            where TInput : class
+            where TOutput : class
+            where TError : class
+    {
+        public HttpStatusCode StatusCode { get; set; }
+        public TInput? Input { get; set; } = null;
+        public TOutput? Output { get; set; } = null;
+        public TError? Error { get; set; } = null;
+        public bool IsOk { get; set; } = true;
+        internal PostResponse() { }
+    }
+
+    public class GetResponse<TOutput, TError>
+        where TOutput : class
+        where TError : class
+    {
+        public HttpStatusCode StatusCode { get; set; }
+        public TOutput? Output { get; set; } = null;
+        public TError? Error { get; set; } = null;
+        public bool IsOk { get; set; } = true;
+        public GetResponse() { }
+    }
+
     public class HttpHelper
     {
         public static string ApiKey { get; private set; }
@@ -43,6 +68,54 @@ namespace AngkorWat.IO.HTTP
             return ret;
         }
 
+        public static async Task<GetResponse<TOutput, TError>> Get<TOutput, TError>(string url)
+            where TOutput : class
+            where TError : class
+        {
+            var ret = new GetResponse<TOutput, TError>();
+            //var response = await Client.GetStringAsync(url);
+            var response = await Client.GetAsync(url);
+            ret.StatusCode = response.StatusCode;
+
+            string result = response.Content.ReadAsStringAsync().Result;
+
+            //var ret = JsonConvert.DeserializeObject<TOutput>(response);
+
+            if (result is null || result.Length == 0)
+            {
+                ret.IsOk = false;
+                return ret;
+            }
+            else if (response.StatusCode == HttpStatusCode.OK)
+            {
+                try
+                {
+                    ret.Output = JsonConvert.DeserializeObject<TOutput>(result);
+                }
+                catch (Exception ex)
+                {
+                    ret.IsOk = false;
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            else
+            {
+                ret.IsOk = false;
+
+                try
+                {
+                    ret.Error = JsonConvert.DeserializeObject<TError>(result);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            return ret;
+        }
+
+
         public static async Task<string> Post<TInput>(string url, TInput dataToSend)
         {
             var json = JsonConvert.SerializeObject(dataToSend);
@@ -71,6 +144,63 @@ namespace AngkorWat.IO.HTTP
             string result = response.Content.ReadAsStringAsync().Result;
 
             var ret = JsonConvert.DeserializeObject<TOutput>(result);
+
+            return ret;
+        }
+
+        public static async Task<PostResponse<TInput, TOutput, TError>> Post<TInput, TOutput, TError>(
+            string url, TInput dataToSend)
+            where TInput : class
+            where TOutput : class
+            where TError : class
+        {
+            var ret = new PostResponse<TInput, TOutput, TError>()
+            {
+                Input = dataToSend,
+            };
+
+            var json = JsonConvert.SerializeObject(dataToSend);
+
+            var data = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var response = await Client.PostAsync(url, data);
+
+            ret.StatusCode = response.StatusCode;
+
+            Console.WriteLine(response.StatusCode);
+
+            string result = response.Content.ReadAsStringAsync().Result;
+
+            if (result is null || result.Length == 0)
+            {
+                ret.IsOk = false;
+                return ret;
+            }
+            else if (response.StatusCode == HttpStatusCode.OK)
+            {
+                try
+                {
+                    ret.Output = JsonConvert.DeserializeObject<TOutput>(result);
+                }
+                catch (Exception ex) 
+                {
+                    ret.IsOk = false;
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            else
+            {
+                ret.IsOk = false;
+
+                try
+                {
+                    ret.Error = JsonConvert.DeserializeObject<TError>(result);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
 
             return ret;
         }
